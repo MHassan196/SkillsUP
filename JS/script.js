2beb943953e056aad409ca848c125bf59f07e9eb
MHassan196 <hssnkhatri2@gmail.com>
2022-09-18 18:03:22 +0500

    Responsive Design Improved and alert also added on course enrollment

--- JS/script.js
+++ JS/script.js
@@ -14,3 +14,28 @@ function slide(direction){
     }, 30);
 }
 
+// const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+
+// const alert = (message, type) => {
+//   const wrapper = document.createElement('div')
+//   wrapper.innerHTML = [
+//     `<div class="alert alert-${type} alert-dismissible" id="course-alert" role="alert">`,
+//     `   <div>${message}</div>`,
+//     '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+//     '</div>'
+//   ].join('')
+
+//   alertPlaceholder.append(wrapper)
+// }
+
+// const alertTrigger = document.getElementById('liveAlertBtn')
+// if (alertTrigger) {
+//   alertTrigger.addEventListener('click', () => {
+//     alert('Nice, you triggered this alert message!', 'success')
+//   })
+// }
+
+// setTimeout(function () {
+//     // Closing the alert
+//     $('.alert').alert('close');
+// }, 10000);
